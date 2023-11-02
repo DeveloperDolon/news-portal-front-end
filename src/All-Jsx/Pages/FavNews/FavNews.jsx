b7f6ec2fd51f23cab73f9665331ce352ef6a9987
Dolon Roy <dolonr718@gmail.com>
2023-11-02 11:57:54 +0600

    fav news delete method added commit

--- src/All-Jsx/Pages/FavNews/FavNews.jsx
+++ src/All-Jsx/Pages/FavNews/FavNews.jsx
@@ -1,12 +1,25 @@
+import axios from "axios";
 import useFavNews from "../../Hooks/useFavNews";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+import toast, { Toaster } from "react-hot-toast";
+import { useContext } from "react";
 
 
 const FavNews = () => {
-
+    const {mainUrl} = useContext(AuthContext);
     const {data} = useFavNews();
+    console.log
+    const handleDelete = (id) => {
+        axios.delete(`${mainUrl}/fav-news/${id}`)
+        .then(res => {
+            if(res.data.acknowledged) {
+                toast.success("Deleted!");
+            }
+        }).catch(err => console.log(err));
+    }
 
     return (
-        <div className="max-w-7xl mx-auto lg:px-0 md:px-5 px-3"> 
+        <div className="max-w-7xl mx-auto lg:px-0 md:px-5 px-3 md:mb-28 mb-20"> 
             <h1 className="md:text-5xl font-bold text-center md:my-16 my-10">All Favorite News</h1>
 
             <div className="grid grid-cols-1 gap-6">
@@ -17,11 +30,13 @@ const FavNews = () => {
                         </div>
 
                         <div className="col-span-3 text-left sm:py-0 py-3">
-                            <h1 className="text-white md:text-xl sm:text-base text-xs font-semibold">{news?.newsTitle}</h1>
+                            <h1 className="text-white md:text-2xl sm:text-lg text-xs font-semibold">{news?.newsTitle}</h1>
                         </div>
+                        <Toaster></Toaster>
+                        <div className="sm:px-0 pr-2 flex flex-col gap-2">
+                            <button onClick={() => handleDelete(news._id)} className="p-[2px] sm:btn-sm col-span-1 btn-primary rounded-lg sm:text-sm text-xs">Remove</button>
+                            <button className="p-[2px] sm:btn-sm col-span-1 bg-[red] text-white rounded-lg sm:text-sm text-xs">Unread</button>
 
-                        <div className="sm:px-0 pr-2">
-                            <button className="p-[2px] sm:btn-sm col-span-1 btn-primary rounded-lg sm:text-sm text-xs">Remove</button>
                         </div>
                     </div>)
                 }
