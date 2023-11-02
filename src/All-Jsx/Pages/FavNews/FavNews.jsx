f730ab7cf4a7253b5e81759e0db3902ff724e81e
Dolon Roy <dolonr718@gmail.com>
2023-11-02 12:42:43 +0600

    read function added commit

--- src/All-Jsx/Pages/FavNews/FavNews.jsx
+++ src/All-Jsx/Pages/FavNews/FavNews.jsx
@@ -1,21 +1,46 @@
 import axios from "axios";
-import useFavNews from "../../Hooks/useFavNews";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
 import toast, { Toaster } from "react-hot-toast";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 
 
 const FavNews = () => {
-    const {mainUrl} = useContext(AuthContext);
-    const {data} = useFavNews();
-    console.log
+    const {mainUrl, user} = useContext(AuthContext);
+    const [favNews, setFavNews] = useState([]);
+
+
+    useEffect(() => {
+        axios.get(`${mainUrl}/fav-news?email=${user?.email}&sort=1`)
+        .then(res => setFavNews(res.data))
+        .catch(err => console.error(err))
+    }, [mainUrl, user?.email]) 
+    
     const handleDelete = (id) => {
         axios.delete(`${mainUrl}/fav-news/${id}`)
         .then(res => {
             if(res.data.acknowledged) {
                 toast.success("Deleted!");
+                const filterNews = favNews.filter(news => news._id !== id);
+                setFavNews(filterNews);
             }
         }).catch(err => console.log(err));
+    }
+
+    const handleRead = (id) => {
+        axios.patch(`${mainUrl}/fav-news/${id}`, {status : true})
+        .then(res => {
+            if(res.data.acknowledged) {
+                toast.success("Reading Success!!");
+                const filterNews = favNews.filter(news => news._id !== id);
+                const thatNews = favNews.find(news => news._id === id);
+                thatNews.status = true;
+                const allNews = [thatNews, ...filterNews];
+                setFavNews(allNews);
+            }
+        }).catch(err => console.log(err.message));
     }
 
     return (
@@ -24,19 +49,25 @@ const FavNews = () => {
 
             <div className="grid grid-cols-1 gap-6">
                 {
-                    data?.map(news => <div className="grid grid-cols-6  sm:gap-8 gap-3 bg-green-400 rounded-xl overflow-hidden place-items-center justify-start" key={news._id}>
+                    favNews?.map(news => <div className="grid grid-cols-6  sm:gap-8 gap-3 bg-green-400 rounded-xl overflow-hidden place-items-center justify-start" key={news._id}>
                         <div className="col-span-2 h-full">
                             <img className="w-full h-full object-cover" src={news?.newsImage} alt="" />
                         </div>
 
                         <div className="col-span-3 text-left sm:py-0 py-3">
-                            <h1 className="text-white md:text-2xl sm:text-lg text-xs font-semibold">{news?.newsTitle}</h1>
+                            <Link to={`/news/${news.newsId}`}>
+                                <h1 className="text-white hover:underline md:text-2xl sm:text-lg text-xs font-semibold">{news?.newsTitle}</h1>
+                            </Link>
                         </div>
                         <Toaster></Toaster>
                         <div className="sm:px-0 pr-2 flex flex-col gap-2">
                             <button onClick={() => handleDelete(news._id)} className="p-[2px] sm:btn-sm col-span-1 btn-primary rounded-lg sm:text-sm text-xs">Remove</button>
-                            <button className="p-[2px] sm:btn-sm col-span-1 bg-[red] text-white rounded-lg sm:text-sm text-xs">Unread</button>
 
+                            {
+                                news?.status ?  <button className="p-[2px] sm:btn-sm col-span-1 bg-green-600 text-white rounded-lg sm:text-sm text-xs">Read Done</button> : 
+                                 <button onClick={() => handleRead(news._id)} className="p-[2px] sm:btn-sm col-span-1 bg-[red] text-white rounded-lg sm:text-sm text-xs">Unread</button>
+                            }
+                            
                         </div>
                     </div>)
                 }
